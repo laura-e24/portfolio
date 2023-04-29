309d038b879d0de6db26278a7f455eacc4bb1589
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2023-04-28 22:45:15 -0300

    remove purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    "postcss-purgecss": {
-      content: ["src/**/*.html,.jsx"],
-      css: ["src/**/*.css"]
-    }
   },
 }
