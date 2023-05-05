b39ef1ec0f25e82031f3efd2c4e352529d4b54da
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2023-05-05 16:05:14 -0300

    add CV

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,23 +1,3 @@
-// import { useState, useEffect, useCallback, useRef } from "react";
-// import Footer from "./Footer";
-// import NavBar from "./NavBar";
-// import Particles from "react-tsparticles";
-// import { loadFull } from "tsparticles";
-// import particlesConfig from "@/data/particlesjs-config.json"
-
-// const Layout = ({ children }) => {
-
-// return (
-//    <>
-
-//    </>
-// );
-// }
- 
-// export default Layout;
-
-
-
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import Particles from "react-tsparticles";
@@ -46,89 +26,87 @@ const Layout = ({ children }) => {
       `}
     </style>
       <main className="relative">
-        {/* <div className="bg"></div> */}
-          <Image
-            src={wavebg}
-            alt=""
-            className="absolute inset-x-0 -z-10 rotate-180 opacity-25 -top-16 hidden md:inline"
-          />
-        
-        <div 
-          className='w-full bg-center bg-cover bg-img-size md:hidden absolute inset-x-0 -z-10 rotate-180 opacity-25' 
-          style={{ backgroundImage: `url(/pattern.svg)`, height: 400}}
-        ></div>
-        <NavBar />
-        <div className='mt-20 md:mt-0'>
-          {children}
-        </div>
-        <Footer />
-         <Particles 
-          id="tsparticles" 
-          options={{
-            fpsLimit: 60,
-            interactivity: {},
-            particles: {
-              "number": {
-                "value": 600,
-                "density": {
-                  "enable": true,
-                  "value_area": 600
-                }
-              },
-              "color": {
-                "value": "#676767"
-              },
-              "shape": {
-                "type": "star",
-                "stroke": {
-                  "width": 0,
-                  "color": "#000000"
-                },
-                "polygon": {
-                  "nb_sides": 5
-                },
-              },
-              "opacity": {
-                "value": 0.3,
-                "random": true,
-                "anim": {
-                  "enable": true,
-                  "speed": 1,
-                  "opacity_min": 0,
-                  "sync": false
-                }
-              },
-              "size": {
-                "value": 1.5,
-                "random": true,
-                "anim": {
-                  "enable": true,
-                  "speed": 2,
-                  "size_min": 0.3,
-                  "sync": false
-                }
-              },
-              "move": {
+        <Image
+          src={wavebg}
+          alt=""
+          className="absolute inset-x-0 -z-10 rotate-180 opacity-25 -top-16 hidden md:inline"
+        />
+      <div 
+        className='w-full bg-center bg-cover bg-img-size md:hidden absolute inset-x-0 -z-10 rotate-180 opacity-25' 
+        style={{ backgroundImage: `url(/pattern.svg)`, height: 400}}
+      ></div>
+      <NavBar />
+      <div className='mt-20 md:mt-0'>
+        {children}
+      </div>
+      <Footer />
+        <Particles 
+        id="tsparticles" 
+        options={{
+          fpsLimit: 60,
+          interactivity: {},
+          particles: {
+            "number": {
+              "value": 600,
+              "density": {
                 "enable": true,
-                "speed": 1,
-                "direction": "none",
-                "random": true,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": false,
-                "attract": {
-                  "enable": false,
-                  "rotateX": 600,
-                  "rotateY": 600
-                }
+                "value_area": 600
               }
             },
-            detectRetina: true,
-          }}
-          init={particlesInit} 
-          // loaded={particlesLoaded} 
-          style={{overflowY: "scroll"}}
-          />
+            "color": {
+              "value": "#676767"
+            },
+            "shape": {
+              "type": "star",
+              "stroke": {
+                "width": 0,
+                "color": "#000000"
+              },
+              "polygon": {
+                "nb_sides": 5
+              },
+            },
+            "opacity": {
+              "value": 0.3,
+              "random": true,
+              "anim": {
+                "enable": true,
+                "speed": 1,
+                "opacity_min": 0,
+                "sync": false
+              }
+            },
+            "size": {
+              "value": 1.5,
+              "random": true,
+              "anim": {
+                "enable": true,
+                "speed": 2,
+                "size_min": 0.3,
+                "sync": false
+              }
+            },
+            "move": {
+              "enable": true,
+              "speed": 1,
+              "direction": "none",
+              "random": true,
+              "straight": false,
+              "out_mode": "out",
+              "bounce": false,
+              "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 600
+              }
+            }
+          },
+          detectRetina: true,
+        }}
+        init={particlesInit} 
+        // loaded={particlesLoaded} 
+        style={{overflowY: "scroll"}}
+        />
       </main>
     </>
   );
