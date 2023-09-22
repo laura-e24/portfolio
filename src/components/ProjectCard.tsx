b8538d770baaef5c159650f81f8a88fb1b57e0e3
Laura Eliceiry <72033353+laura-e24@users.noreply.github.com>
2023-09-22 19:31:40 -0300

    Update ProjectCard.tsx

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
 import { techFullName } from '@/utils/general';
 import { Tooltip } from 'react-tooltip';
-//import SIcon from './SIcon';
-import FAIcon from './FAIcon';
 import dynamic from 'next/dynamic'
  
 const SIcon = dynamic(() => import('./SIcon'))
+const FAIcon = dynamic(() => import('./FAIcon'))
 
 const ProjectCard = ({ project }) => {
   return (  
