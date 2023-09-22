a0631aeb8798de420e7f493fc224ab33bcd41352
Laura Eliceiry <72033353+laura-e24@users.noreply.github.com>
2023-09-22 19:04:53 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import dynamic from 'next/dynamic'
 //import '@/styles/globals.css'
-dynamic(() => import("../styles/globals.css"))
+dynamic(() => import("@/styles/globals.css"))
 import type { AppProps } from 'next/app'
 //import "../styles/css/all.min.css"
-dynamic(() => import("../styles/css/all.min.css"))
+dynamic(() => import("@/styles/css/all.min.css"))
 //import 'react-tooltip/dist/react-tooltip.css'
 dynamic(() => import("react-tooltip/dist/react-tooltip.css"))
 
