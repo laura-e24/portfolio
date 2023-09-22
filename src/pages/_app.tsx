0f9a1bf6b5fcdaf3b635e0dd9d8b449706179e8b
Laura Eliceiry <72033353+laura-e24@users.noreply.github.com>
2023-09-22 19:11:40 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,7 @@ import dynamic from 'next/dynamic'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import "../styles/css/all.min.css"
-//import 'react-tooltip/dist/react-tooltip.css'
-dynamic(() => import("react-tooltip/dist/react-tooltip.css"))
+import 'react-tooltip/dist/react-tooltip.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
