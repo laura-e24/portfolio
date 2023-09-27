1592b9af6a42263a82aa5411b11e98175b82e739
laura-e24 <novadeargon@gmail.com>
2023-09-26 22:11:54 -0300

    probar dynamic import img projects finished

--- src/data/index.ts
+++ src/data/index.ts
@@ -160,81 +160,102 @@ export const projects = [
     deploy: "https://marvelscharacters.vercel.app",
     date: "Abril 2023"
   }, 
-  // {
-  //   name: "Thumbnail Generator",
-  //   description: "API REST que recibe una imagen PNG o JPEG cargada desde el ordenador y a partir de ella genera tres thumbnails con distintos tamaños, los guarda en el ordenador y retorna las URLs de donde se almacenaron.",
-  //   thumbnail: "thumbnailgen.png",
-  //   tech: ["node.js", "typescript", "express", "swagger", "mocha"],
-  //   status: "Terminado",
-  //   type: "Prueba técnica",
-  //   repo: "https://github.com/laura-e24/proof-of-concept",
-  //   deploy: null,
-  //   date: "Marzo 2023"
-  // },
-  // {
-  //   name: "FormatCSV App",
-  //   description: "Extrae datos en formato CSV de API externa, la formatea a JSON y el resultado es mostrado en pantalla en formato tabla con diseño sencillo y minimalista. REST API con testing y documentación.",
-  //   thumbnail: "formatcsv.png",
-  //   tech: ["node.js", "react", "css3", "bootstrap", "express", "mocha", "swagger"],
-  //   status: "Terminado",
-  //   type: "Prueba técnica",
-  //   repo: "https://github.com/laura-e24/formatCSV-app",
-  //   deploy: null,
-  //   date: "Enero 2023"
-  // }, 
-  // {
-  //   name: "La Dionisia",
-  //   description: "Vinoteca online — ecommerce — con un amplio catálogo de distintos vinos traídos de API propia construida con PHP y MySQL. Incluye múltiples filtros y ordenamientos combinados, registro y auntenticación, guardado de favoritos, plataforma de pago que admite PayPal y tarjetas de crédito/débito, y rol de administrador para gestionar la tienda. UI/UX totalmente responsive, agradable y con múltiples funcionalidades de accesibilidad.",
-  //   thumbnail: "dionisia.png",
-  //   tech: ["node.js", "react", "typescript", "nextdotjs", "express", "css3", "tailwindcss", "redux", "postgresql", "auth0", "stripe", "figma", "php", "mysql"],
-  //   status: "Terminado",
-  //   type: "Proyecto académico grupal",
-  //   repo: "https://github.com/laura-e24/LaDionisiaFront",
-  //   deploy: "https://la-dionisia-front.vercel.app",
-  //   date: "Enero 2023"
-  // }, 
-  // {
-  //   name: "Henry Countries",
-  //   description: "Extrae de una API externa y guarda en base de datos la información de los países del mundo, permitiendo ver todos sus detalles y gestionar actividades turísticas; creando, editando y eliminándolas. Incluye búsqueda y múltiples filtros y ordenamientos combinados para facilitar la selección de países.",
-  //   thumbnail: "PI.png",
-  //   tech: ["node.js", "vite", "react", "express", "css3", "styledcomponents", "redux", "postgresql", "figma"],
-  //   status: "Activo",
-  //   type: "Proyecto académico",
-  //   repo: "https://github.com/laura-e24/PI-Countries",
-  //   deploy: "https://henrycountries.vercel.app",
-  //   date: "Noviembre ─ diciembre 2022"
-  // }, 
-  // {
-  //   name: "UpTask",
-  //   description: "Admistrador de proyectos y tareas. Permite crearnos una cuenta con email y contraseña, validando los datos para la autenticación mediante JSON Web Token, pudiendo además restablecer la clave en caso de olvidarla. Al mismo tiempo, cada vez que registremos una cuenta nueva o queramos restablecer la contraseña, nos llegará un mail para confirmar el cambio. Permite crear tantos proyectos como queramos, con tantas tareas como queramos; pudiendo editarlas, eliminarlas o marcarlas como completadas. A su vez, se pueden agregar varios colaboradores a los proyectos, de modo que sólo quienes tengan acceso al mismo podrán verlo e interactuar con él.",
-  //   thumbnail: "uptask.png",
-  //   tech: ["node.js", "react", "typescript", "vite", "express", "css3", "tailwindcss", "redux", "mongodb", "jsonwebtokens"],
-  //   status: "En curso",
-  //   type: "Proyecto académico (curso)",
-  //   repo: "https://github.com/laura-e24/UpTask",
-  //   deploy: null,
-  //   date: "2022 ─ actualidad"
-  // }, 
-  // {
-  //   name: "Cotizador de seguros de automóviles",
-  //   description: "Aplicación sencilla que simula un cotizador de seguro para automóviles, pudiendo elegir marca y año del auto y tipo de seguro deseado, para luego calcular el precio según las especificaciones seleccionadas.",
-  //   thumbnail: "cotizador.png",
-  //   tech: ["node.js", "react", "css3", "skeleton"],
-  //   status: "Terminado",
-  //   type: "Proyecto académico (curso)",
-  //   repo: null,
-  //   deploy: "https://cotizadorsegurosreact-laura-e24.netlify.app",
-  //   date: "2021"
-  // },
-  // {
-  //   name: "Calculadora de presupuesto",
-  //   description: "Sencilla calculadora para llevar registro de nuestro presupuesto y gastos. UI/UX simple e intuitiva.",
-  //   thumbnail: "presupuesto.png",
-  //   tech: ["node.js", "react", "css3", "skeleton"],
-  //   status: "Terminado",
-  //   type: "Proyecto académico (curso)",
-  //   repo: null,
-  //   deploy: "https://calcularpresupuestoreact-laura-e24.netlify.app",
-  //   date: "2021"
-  // }
+  {
+    name: "Thumbnail Generator",
+    description: "API REST que recibe una imagen PNG o JPEG cargada desde el ordenador y a partir de ella genera tres thumbnails con distintos tamaños, los guarda en el ordenador y retorna las URLs de donde se almacenaron.",
+    thumbnail: async () => {
+      const res = await import("../../public/projects/thumbnailgen.png")
+      return res.default.src
+    },
+    tech: ["node.js", "typescript", "express", "swagger", "mocha"],
+    status: "Terminado",
+    type: "Prueba técnica",
+    repo: "https://github.com/laura-e24/proof-of-concept",
+    deploy: null,
+    date: "Marzo 2023"
+  },
+  {
+    name: "FormatCSV App",
+    description: "Extrae datos en formato CSV de API externa, la formatea a JSON y el resultado es mostrado en pantalla en formato tabla con diseño sencillo y minimalista. REST API con testing y documentación.",
+    thumbnail: async () => {
+      const res = await import("../../public/projects/formatcsv.png")
+      return res.default.src
+    },
+    tech: ["node.js", "react", "css3", "bootstrap", "express", "mocha", "swagger"],
+    status: "Terminado",
+    type: "Prueba técnica",
+    repo: "https://github.com/laura-e24/formatCSV-app",
+    deploy: null,
+    date: "Enero 2023"
+  }, 
+  {
+    name: "La Dionisia",
+    description: "Vinoteca online — ecommerce — con un amplio catálogo de distintos vinos traídos de API propia construida con PHP y MySQL. Incluye múltiples filtros y ordenamientos combinados, registro y auntenticación, guardado de favoritos, plataforma de pago que admite PayPal y tarjetas de crédito/débito, y rol de administrador para gestionar la tienda. UI/UX totalmente responsive, agradable y con múltiples funcionalidades de accesibilidad.",
+    thumbnail: async () => {
+      const res = await import("../../public/projects/dionisia.png")
+      return res.default.src
+    },
+    tech: ["node.js", "react", "typescript", "nextdotjs", "express", "css3", "tailwindcss", "redux", "postgresql", "auth0", "stripe", "figma", "php", "mysql"],
+    status: "Terminado",
+    type: "Proyecto académico grupal",
+    repo: "https://github.com/laura-e24/LaDionisiaFront",
+    deploy: "https://la-dionisia-front.vercel.app",
+    date: "Enero 2023"
+  }, 
+  {
+    name: "Henry Countries",
+    description: "Extrae de una API externa y guarda en base de datos la información de los países del mundo, permitiendo ver todos sus detalles y gestionar actividades turísticas; creando, editando y eliminándolas. Incluye búsqueda y múltiples filtros y ordenamientos combinados para facilitar la selección de países.",
+    thumbnail: async () => {
+      const res = await import("../../public/projects/PI.png")
+      return res.default.src
+    },
+    tech: ["node.js", "vite", "react", "express", "css3", "styledcomponents", "redux", "postgresql", "figma"],
+    status: "Activo",
+    type: "Proyecto académico",
+    repo: "https://github.com/laura-e24/PI-Countries",
+    deploy: "https://henrycountries.vercel.app",
+    date: "Noviembre ─ diciembre 2022"
+  }, 
+  {
+    name: "UpTask",
+    description: "Admistrador de proyectos y tareas. Permite crearnos una cuenta con email y contraseña, validando los datos para la autenticación mediante JSON Web Token, pudiendo además restablecer la clave en caso de olvidarla. Al mismo tiempo, cada vez que registremos una cuenta nueva o queramos restablecer la contraseña, nos llegará un mail para confirmar el cambio. Permite crear tantos proyectos como queramos, con tantas tareas como queramos; pudiendo editarlas, eliminarlas o marcarlas como completadas. A su vez, se pueden agregar varios colaboradores a los proyectos, de modo que sólo quienes tengan acceso al mismo podrán verlo e interactuar con él.",
+    thumbnail: async () => {
+      const res = await import("../../public/projects/uptask.png")
+      return res.default.src
+    },
+    tech: ["node.js", "react", "typescript", "vite", "express", "css3", "tailwindcss", "redux", "mongodb", "jsonwebtokens"],
+    status: "En curso",
+    type: "Proyecto académico (curso)",
+    repo: "https://github.com/laura-e24/UpTask",
+    deploy: null,
+    date: "2022 ─ actualidad"
+  }, 
+  {
+    name: "Cotizador de seguros de automóviles",
+    description: "Aplicación sencilla que simula un cotizador de seguro para automóviles, pudiendo elegir marca y año del auto y tipo de seguro deseado, para luego calcular el precio según las especificaciones seleccionadas.",
+    thumbnail: async () => {
+      const res = await import("../../public/projects/cotizador.png")
+      return res.default.src
+    },
+    tech: ["node.js", "react", "css3", "skeleton"],
+    status: "Terminado",
+    type: "Proyecto académico (curso)",
+    repo: null,
+    deploy: "https://cotizadorsegurosreact-laura-e24.netlify.app",
+    date: "2021"
+  },
+  {
+    name: "Calculadora de presupuesto",
+    description: "Sencilla calculadora para llevar registro de nuestro presupuesto y gastos. UI/UX simple e intuitiva.",
+    thumbnail: async () => {
+      const res = await import("../../public/projects/presupuesto.png")
+      return res.default.src
+    },
+    tech: ["node.js", "react", "css3", "skeleton"],
+    status: "Terminado",
+    type: "Proyecto académico (curso)",
+    repo: null,
+    deploy: "https://calcularpresupuestoreact-laura-e24.netlify.app",
+    date: "2021"
+  }
 ]
